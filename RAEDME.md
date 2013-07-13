24c47ac7b740e4e96e98464e07e823c07069e4a3
Sho SAKAMOTO <sho.skmt1220+github@gmail.com>
2013-07-14 01:48:26 +0900

    change readme file

---
+++ RAEDME.md
@@ -0,0 +1,21 @@
+#jquery-float-area.js
+You can use this plug-in, you will be able to move as you move the mouse the range that you specify.
+
+use
+```javascript:jquery-float-area.js
+$(function() {
+    $(element).floatArea();
+});
+```
+
+default
+* top: 100px
+* duration : 900
+
+set options
+```javascript:jquery-float-area.js
+$(function() {
+    $(element).floatArea({top:30, duration:500});
+});
+```
+
---
+++ jquery-float-area.js
@@ -0,0 +1,29 @@
+(function ($) {
+
+	$.fn.floatArea = function(options) {
+		var defaults = {
+			"duration" : 900,
+			"top" : 100
+		}
+		var setting = $.extend(defaults, options);
+		
+		var name = this.selector;
+		console.log(name);
+		console.log(setting['duration']);
+		$(name).css("top", setting['top']);
+		$(name).css("position", "absolute");
+		
+		var y = parseInt($(name).css("top").substring(0,$(name).css("top").indexOf("px")));
+
+		$(window).scroll(function () {
+			var offset = y + $(document).scrollTop() + "px";
+			$(name).animate({
+				top:offset
+				}, {
+				duration: setting['duration'],
+				queue:false
+			});
+		});
+	}
+	
+})(jQuery);
